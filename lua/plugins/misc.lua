546d5ea23c4bc13dc98ba953334c2d1028a18940
cliuj <jliu2179@gmail.com>
2023-06-20 00:10:34 -0400

    remove(neorg): Remove keybinds on neorg plugin

--- lua/plugins/misc.lua
+++ lua/plugins/misc.lua
@@ -43,25 +43,6 @@ return {
           -- ["core.ui.calendar"] = {}, -- Need neovim@v0.10.0
         },
       }
-
-       local neorg_callbacks = require("neorg.callbacks")
-       neorg_callbacks.on_event("core.keybinds.events.enable_keybinds", function(_, keybinds)
-           -- Map all the below keybinds only when the "norg" mode is active
-           keybinds.map_event_to_mode("norg", {
-               -- Bind keys in normal mode
-               n = {
-                   { "<C-s>", "core.integrations.telescope.find_linkable" },
-               },
-               -- Bind in insert mode
-               i = {
-                   { "<C-s>", "core.integrations.telescope.insert_link" },
-                   { "<C-f>", "core.integrations.telescope.insert_file_link" },
-               },
-           }, {
-               silent = true,
-               noremap = true,
-           })
-       end)
     end,
     dependencies = {
       { "nvim-lua/plenary.nvim" },
