4b53729afdffb39ca4bb7eea4a3ec0dad96783aa
cliuj <jliu2179@gmail.com>
2023-06-19 23:38:35 -0400

    feat(plugin): Add indentscope plugin

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -115,4 +115,26 @@ return {
       },
     },
   },
+  {
+    "echasnovski/mini.indentscope",
+    version = "0.9.0",
+    event = { "BufReadPre", "BufNewFile" },
+    opts = {
+      symbol = "│",
+      options = { try_as_border = true },
+    },
+    init = function()
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = {
+          "help",
+          "neo-tree",
+          "lazy",
+          "mason",
+        },
+        callback = function()
+          vim.b.miniindentscope_disable = true
+        end,
+      })
+    end,
+  },
 }
