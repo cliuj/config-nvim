4b04a4c211ebe4df6e5d5194d959dac959bae53b
cliuj <jliu2179@gmail.com>
2023-06-25 00:01:41 -0400

    feat(plugin, cmp): Update cmp to use all buffers

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -59,7 +59,14 @@ return {
           { name = "nvim_lsp" },
           { name = "nvim_lua" },
           { name = "luasnip" },
-          { name = "buffer" },
+          {
+            name = "buffer",
+            option = {
+              get_bufnrs = function()
+                return vim.api.nvim_list_bufs()
+              end
+            }
+          },
           { name = "path" },
           { name = "neorg"},
         }),
