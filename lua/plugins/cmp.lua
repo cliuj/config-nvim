fa777021602632b96af2140c359d9b173a76f380
cliuj <jliu2179@gmail.com>
2023-07-05 23:44:39 -0400

    feat(cmp): Use C-n/p to navigate docs

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -46,8 +46,8 @@ return {
         mapping = cmp.mapping.preset.insert({
           ["<Tab>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
           ["<S-Tab>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
-          ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-          ["<C-f>"] = cmp.mapping.scroll_docs(4),
+          ["<C-p>"] = cmp.mapping.scroll_docs(-4),
+          ["<C-n>"] = cmp.mapping.scroll_docs(4),
           ["<C-Space>"] = cmp.mapping.complete(),
           ["<C-e>"] = cmp.mapping.abort(),
           ["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
