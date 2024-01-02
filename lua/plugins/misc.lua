b1260bb1b9da1cac7b9a00fea7934399882aefac
cliuj <jliu2179@gmail.com>
2024-01-02 18:14:45 -0500

    feat(plugin): Add nvim-surround plugin

--- lua/plugins/misc.lua
+++ lua/plugins/misc.lua
@@ -71,4 +71,14 @@ return {
       require('bufterm').setup()
     end,
   },
+  {
+    "kylechui/nvim-surround",
+    version = "*", -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
+    config = function()
+        require("nvim-surround").setup({
+            -- Configuration here, or leave empty to use defaults
+        })
+    end
+  }
 }
