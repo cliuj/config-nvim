b4f9901b6b47989125e207d727cc56d3b6a69afc
cliuj <jliu2179@gmail.com>
2024-01-02 18:05:34 -0500

    refactor(colorscheme): Remove deprecated vim-dark-meadow colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,11 +1,5 @@
 return {
   {
-    "cliuj/vim-dark-meadow",
-    version = "1.0.0",
-    lazy = false,
-    priority = 1000,
-  },
-  {
     "cliuj/midnight-meadow.nvim",
     priority = 1000,
     lazy = true,
