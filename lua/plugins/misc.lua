85f19c9b6b0dec1997228f83b0b061c86911feab
Charlie Liu <cliuj.dev@outlook.com>
2026-01-27 01:05:17 -0500

    chore(markview): Enable blink for markview

--- lua/plugins/misc.lua
+++ lua/plugins/misc.lua
@@ -20,7 +20,7 @@ return {
     "OXY2DEV/markview.nvim",
     lazy = false,
     -- Completion for `blink.cmp`
-    -- dependencies = { "saghen/blink.cmp" },
+    dependencies = { "saghen/blink.cmp" },
   },
   {
     "vhyrro/luarocks.nvim",
