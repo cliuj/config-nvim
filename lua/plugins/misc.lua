ace5100e8d81682e3da670621d9e5fdc166bc752
Charlie Liu <cliuj.dev@outlook.com>
2026-01-27 02:11:57 -0500

    feat(plugins): Add persistence.nvim for session management
    - Auto-saves session on exit
    - <leader>qs to restore session for current directory
    - <leader>ql to restore last session (any directory)
    - <leader>qS to select from saved sessions
    - <leader>qd to skip saving session on exit

--- lua/plugins/misc.lua
+++ lua/plugins/misc.lua
@@ -116,4 +116,17 @@ return {
     },
     opts = {},
   },
+
+  -- Session management (auto-save on exit, manual restore)
+  {
+    "folke/persistence.nvim",
+    event = "BufReadPre",
+    opts = {},
+    keys = {
+      { "<leader>qs", function() require("persistence").load() end, desc = "Restore session (cwd)" },
+      { "<leader>ql", function() require("persistence").load({ last = true }) end, desc = "Restore last session" },
+      { "<leader>qS", function() require("persistence").select() end, desc = "Select session" },
+      { "<leader>qd", function() require("persistence").stop() end, desc = "Don't save session" },
+    },
+  },
 }
